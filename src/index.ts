7c3de7ca0859411ea5ef8780bd4b5cb43be4267f
yoojm4718 <yoojm4718@outlook.com>
2021-09-22 18:07:34 +0900

    Example of Using Class in TS

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,22 @@
-const name = "Jungmin",
-  age = 20,
-  gender = "male";
+class Human {
+  public name: string;
+  public age: number;
+  public gender: string;
+  constructor(name: string, age: number, gender: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
+}
 
-const sayHi = (name, age, gender?) => {
-  console.log(`Hello ${name}, you are ${age} and ${gender}`);
+const jungmin = new Human("Jungmin", 20, "male");
+
+const sayHi = (person: Human) => {
+  console.log(
+    `Hello ${person.name}, you are ${person.age} and ${person.gender}`
+  );
 };
 
-sayHi(name, age, gender);
+sayHi(jungmin);
 
 export {};
